e0c1bc71d71742f5d7724a9e3f99b8020ad9ee4e
domin <dominikpolakcz@gmail.com>
2016-12-03 10:35:09 +0100

    ...

--- sketch_dec02b/sketch_dec02b.ino
+++ sketch_dec02b/sketch_dec02b.ino
@@ -18,16 +18,33 @@ float pwd2 = 0;
 //RAND_MODE values
 int tmp1 = 2000;
 int tmp2 = 5000;
+//button values
+int previous = 0;
 
 void setup() {
   pinMode(FotoR1pin,INPUT);
   pinMode(FotoR2pin,INPUT);
+  pinMode(8,INPUT);
   pinMode(LED1pin,OUTPUT);
   pinMode(LED2pin,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
+  if(digitalRead(8)==1){
+    if(previous==0){
+      if(mode==2){
+        mode=0;
+      }
+      else mode++;
+    }
+    previous=1;
+  }
+  else
+  {
+    previous=0;
+  }
+  
   if(mode==1){
     on();
   }else if(mode==2){
@@ -51,12 +68,12 @@ void ran(){
     if(a1<0){
       if(pwd1==0.9)pwd1=0;
       else pwd1=0.9;
-      tmp1+=random(1000,10000);
+      tmp1+=random(1000,7000);
     }
     if(a2<0){
       if(pwd2==0.9)pwd2=0;
       else pwd2=0.9;
-      tmp2+=random(1000,10000);
+      tmp2+=random(1000,7000);
     }
     
     led(LED2pin, pwd2);
